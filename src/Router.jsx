c9e3e15b9815a1c88142a1db8d910ebf4bf6cd8e
singsangssong <plmko0914@gmail,com>
2024-07-04 22:51:29 +0900

    start: hello world

---
+++ src/Router.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, NavLink, Link } from "react-router-dom";
+import Test1 from "./test/Test1";
+import Test2 from "./test/Test2";
+import Test3 from "./test/Test3";
+import Test4 from "./test/Test4";
+import Role from "./test/Role"
+
+export default function Router() {
+    return (
+        <BrowserRouter>
+                 
+        </BrowserRouter>
+    );
+}
---
+++ src/test/Test3.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Test3() {
+  return <h1>Test3</h1>;
+}

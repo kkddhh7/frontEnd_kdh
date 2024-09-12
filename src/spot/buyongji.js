43fab444c71168b262f2a5d376d6fc761f461ded
GemGemMin <kkm32543@khu.ac.kr>
2024-09-12 14:17:51 +0900

    modify Buyongji Cat Animation

--- src/spot/buyongji.js
+++ src/spot/buyongji.js
@@ -4,9 +4,8 @@ import cat1 from './image/buyongji/buyongji_cat_icon1.png';
 import cat2 from './image/buyongji/buyongji_cat_icon2.png';
 import cat3 from './image/buyongji/buyongji_cat_icon3.png';
 import king from './image/buyongji/buyongji_king.png';
-import CloudAnimation from './backgroundAnimation'; // CloudAnimation 컴포넌트 import
+import BackgroundAnimation from './backgroundAnimation';
 import book from './image/buyongji/book_buyongji.png';
-
 
 export default function Buyongji() {
   const [currentCat, setCurrentCat] = useState(cat1);
@@ -14,8 +13,7 @@ export default function Buyongji() {
   const [isCatVisible, setIsCatVisible] = useState(true);
   const [showBook, setShowBook] = useState(true);
   
-  const cats = useMemo(() => [cat1, cat2, cat3, cat2], []); // useMemo로 cats 배열 메모이제이션
-  
+  const cats = useMemo(() => [cat1, cat2, cat3, cat2], []);
   const currentIndex = useRef(0);
 
   useEffect(() => {
@@ -25,7 +23,7 @@ export default function Buyongji() {
     }, 500); 
 
     return () => clearInterval(interval);
-  }, [cats]); // cats는 이제 메모이제이션 되어 있으므로 안전함
+  }, [cats]);
 
   const handleIconClick = () => {
     setIsKingVisible(true);
@@ -34,13 +32,12 @@ export default function Buyongji() {
 
   const handleBookClick = () => {
     setShowBook(false);
-};
-
+  };
 
   const catStyles = {
-    cat1: { width: '133px', position: 'absolute', top: '35.8%', left: '55.34%' , zIndex:3},
-    cat2: { width: '190px', position: 'absolute', top: '35.5%', left: '53.1%', zIndex:3 },
-    cat3: { width: '180px', position: 'absolute', top: '34.1%', left: '53.75%', zIndex:3 },
+    cat1: { width: '133px', position: 'absolute', top: '359px', left: '958px', zIndex: 3 },
+    cat2: { width: '190px', position: 'absolute', top: '355px', left: '900px', zIndex: 3 },
+    cat3: { width: '180px', position: 'absolute', top: '337px', left: '917px', zIndex: 3 },
   };
 
   const getCurrentCatStyle = () => {
@@ -51,38 +48,49 @@ export default function Buyongji() {
 
   return (
     <div style={{ position: 'relative', overflow: 'hidden', height: '1069px', width: '1710px' }}>
-      <CloudAnimation/>
-      <img src={buyongji} alt="부용지" style={{ position: 'absolute', top: 0, left: 0, width: '1710px', 
-          height: '1069px', zIndex: 1,objectFit: 'cover',
-          filter: isKingVisible ? 'brightness(0.5)' : 'brightness(1)', 
-          transition: 'filter 0.5s'  }} />
-  
+      <BackgroundAnimation />
+      <img src={buyongji} alt="부용지" style={{ position: 'absolute', top: 0, left: 0, width: '1710px', height: '1069px', zIndex: 1, objectFit: 'cover', transition: 'filter 0.5s' }} />
+
       {isCatVisible && (
         <div onClick={handleIconClick}>
           <img src={currentCat} alt="고양이" style={getCurrentCatStyle()} />
         </div>
       )}
+
       {showBook && (
-                <div style={{ position: 'absolute', top: '75%', left: '5%', zIndex:3}} onClick={handleBookClick}>
-                    <img src={book} alt="서책" style={{ width: '180px', cursor: 'pointer' }} />
-                </div>
+        <div style={{ position: 'absolute', top: '75%', left: '5%', zIndex: 3 }} onClick={handleBookClick}>
+          <img src={book} alt="서책" style={{ width: '180px', cursor: 'pointer' }} />
+        </div>
       )}
-  
+
       {isKingVisible && (
-        <img 
-          src={king} 
-          alt="왕" 
-          style={{ 
-            position: 'absolute', 
-            top: '50%', 
-            left: '30%', 
-            transform: 'translate(-50%, -50%)', 
-            width: '1000px', 
-            opacity: 1, 
-            transition: 'opacity 10s',
-            animation: 'fadeIn 1s forwards' 
-          }} 
-        />
+        <>
+          <div style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(0, 0, 0, 0.5)', // 반투명 검정색
+            zIndex: 2,
+            transition: 'opacity 0.5s',
+          }} />
+          <img 
+            src={king} 
+            alt="왕" 
+            style={{ 
+              position: 'absolute', 
+              top: '50%', 
+              left: '30%', 
+              transform: 'translate(-50%, -50%)', 
+              width: '1000px', 
+              opacity: 1, 
+              transition: 'opacity 10s',
+              animation: 'fadeIn 1s forwards',
+              zIndex: 3 
+            }} 
+          />
+        </>
       )}
     </div>
   );
